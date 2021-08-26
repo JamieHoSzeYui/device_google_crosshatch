36e82fa06755b9e95d1e114ae193079146532697
Wiley Lau <henloboii@protonmail.ch>
2021-08-26 11:37:49 +0800

    bluecross: back to AOSP
    * we have a separate tree
    
    Signed-off-by: Wiley Lau <henloboii@protonmail.ch>

--- aosp_crosshatch.mk
+++ aosp_crosshatch.mk
@@ -18,6 +18,11 @@
 # All components inherited here go to system image
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+
+# Enable mainline checking
+# TODO(b/138706293): enable Enable mainline checking later
+#PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 
 #
 # All components inherited here go to system_ext image
@@ -26,27 +31,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 #
+# All components inherited here go to product image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+
+#
 # All components inherited here go to vendor image
 #
 # TODO(b/136525499): move *_vendor.mk into the vendor makefile later
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
-# Inherit AOSP stuff
-# $(call inherit-product, vendor/aosp/config/common_telephony.mk)
 $(call inherit-product, device/google/crosshatch/device-crosshatch.mk)
 $(call inherit-product-if-exists, vendor/google_devices/crosshatch/proprietary/device-vendor.mk)
 
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
+# STOPSHIP deal with Qualcomm stuff later
+# PRODUCT_RESTRICT_VENDOR_FILES := all
+
+PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+
+
 PRODUCT_MANUFACTURER := Google
-PRODUCT_BRAND := Google
 PRODUCT_NAME := aosp_crosshatch
 PRODUCT_DEVICE := crosshatch
-PRODUCT_MODEL := Pixel 3 XL
-
-BUILD_FINGERPRINT := "google/crosshatch/crosshatch:11/RQ3A.210705.001/7380771:user/release-keys"
-
-# GMS
-WITH_GMS := true
-
-$(call inherit-product, vendor/google/crosshatch/crosshatch-vendor.mk)
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on crosshatch
